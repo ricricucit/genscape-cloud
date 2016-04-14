3533621525617fa8b7d20a0fc328163f716f95af
Enrico Icardi <enrico.icardi@gmail.com>
2016-04-14 18:44:07 +0200

    ~ added some TURN servers

--- public/javascripts/cloud.js
+++ public/javascripts/cloud.js
@@ -37,18 +37,8 @@ var Cloud = (function(Utils, Drawer) {
                                                     { 'url': 'stun:stun.voxgratia.org' },
                                                     {
                                                       url: 'turn:numb.viagenie.ca',
-                                                      credential: 'muazkh',
-                                                      username: 'webrtc@live.com'
-                                                    },
-                                                    {
-                                                      url: 'turn:192.158.29.39:3478?transport=udp',
-                                                      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                                                      username: '28224511:1379330808'
-                                                    },
-                                                    {
-                                                      url: 'turn:192.158.29.39:3478?transport=tcp',
-                                                      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                                                      username: '28224511:1379330808'
+                                                      credential: 'Ricucit2.',
+                                                      username: 'enrico.icardi@gmail.com'
                                                     }
                                                   ]}
                                         });
