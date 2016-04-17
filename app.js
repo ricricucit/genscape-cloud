8778e89d2edd05a7b147e246b500bf878d263c4b
Enrico Icardi <enrico.icardi@gmail.com>
2016-04-17 20:58:08 +0200

    weird change because of heroku not working and local working...or viceversa

--- app.js
+++ app.js
@@ -50,7 +50,17 @@ var ExpressPeerServer = require('peer').ExpressPeerServer;
 
 var io_cloud = require('socket.io')(https_cloud);
 
-
+if(process.env.PORT){
+  app.listen(regular_port);
+}else{
+  //start express LIVE
+  https_cloud.listen(regular_port, function(){
+    console.log('CLOUD listening events on heroku?:9000');
+  }).on('error', function(err) {
+    console.log('\n------------------------------------\nNetworking ERROR.\nCannot listen to: cloud:' + regular_port + '\nPlease check your Network settings\n------------------------------------\n');
+    process.exit();
+  });
+}
 
 
 
@@ -64,14 +74,6 @@ https_stream_cloud.listen(3002, function(){
 });
 
 app.use('/rt', ExpressPeerServer(https_stream_cloud, {debug: 3}));
-
-//start express LIVE
-https_cloud.listen(regular_port, function(){
-  console.log('CLOUD listening events on heroku?:9000');
-}).on('error', function(err) {
-  console.log('\n------------------------------------\nNetworking ERROR.\nCannot listen to: cloud:' + regular_port + '\nPlease check your Network settings\n------------------------------------\n');
-  process.exit();
-});
 
 
 
