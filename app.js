62a547f162dd91fcc280c49547368901246cd94f
Enrico Icardi <enrico.icardi@gmail.com>
2016-04-17 22:01:38 +0200

    testing essentials for heroku - still not working

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ var regular_port = (process.env.PORT || 9000);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(__dirname + '/public'));
